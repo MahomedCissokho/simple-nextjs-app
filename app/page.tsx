a62a1513f92e082421942d63e1600bdb16598ab0
Mahomed CISSOKHO <mahomedcissokho@esp.sn>
2024-10-02 20:42:01 +0000

    new changes

--- app/page.tsx
+++ app/page.tsx
@@ -3,5 +3,5 @@ export const metadata = {
 };
 
 export default function Page() {
-  return <h1>Hello, my name is Mahomed CISSOKHO</h1>
+  return <h1>Hello, my name is Mahomed CISSOKHO, T'ES LE BEST </h1>
 }
